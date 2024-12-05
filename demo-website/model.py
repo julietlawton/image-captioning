6f1920bf9bc800e91795783522f9434c4c162c4e
Juliet Lawton <124414966+julietlawton@users.noreply.github.com>
2024-12-04 20:54:49 -0800

    Rename home page

--- demo-website/model.py
+++ demo-website/model.py
@@ -25,7 +25,7 @@ densenet121 = DenseNet121(
 tokenizer = captioning_model.tokenizer
 index_to_word = {i: w for i, w in enumerate(tokenizer.get_vocabulary())}
 
-def predict(image):
+def greedy_predict(image):
     image = preprocess_input_densenet(
         tf.expand_dims(
             tf.image.resize(image, [224, 224]), 
@@ -64,6 +64,74 @@ def predict(image):
 
     return caption, [layer.last_attention_scores for layer in captioning_model.decoder_block]
 
+def beamsearch_predict(image, num_beams):
+
+    image = preprocess_input_densenet(
+        tf.expand_dims(
+            tf.image.resize(image, [224, 224]), 
+            axis=0
+        )
+    )
+
+    image_features = densenet121(image)
+
+    # Set the start and end sequence tokens and initialize candidate sequences
+    start_token = tokenizer(['startseq']).numpy()[0][0]
+    end_token =  tokenizer(['endseq']).numpy()[0][0]
+    sequences = [[start_token]]
+    scores = [0.0]
+
+    max_caption_length = 40
+    # Run until sequences maximum length or all have an end token
+    for _ in range(max_caption_length - 1):
+      all_candidates = []
+      for seq, score in zip(sequences, scores):
+
+        # If this sequence is already complete, move to the next one
+        if seq[-1] == end_token:
+          all_candidates.append((seq, score))
+          continue
+
+        # Generate predictions for the next token in the sequence
+        tokens = tf.constant([seq + [0] * (max_caption_length - len(seq))])
+        preds = captioning_model((image_features, tokens)).numpy()
+
+        preds = preds[:, len(seq) - 1, :]
+
+        # Convert the raw logits to probabilities
+        probs = tf.nn.softmax(preds[0])
+        log_probs = tf.math.log(probs)
+
+        # Take the k tokens with the highest probabilities
+        top_k = tf.math.top_k(log_probs, k=num_beams)
+        top_k_tokens = top_k.indices.numpy()
+        top_k_probs = top_k.values.numpy()
+
+        # For each of the top k tokens, build out a candidate sequence using
+        # that token and add its probability to the overall score
+        for token, prob in zip(top_k_tokens, top_k_probs):
+          candidate_seq = seq + [token]
+          candidate_score = score + prob
+          all_candidates.append((candidate_seq, candidate_score))
+
+        # Sort the candidate sequences by their joint probability scores
+        ordered = sorted(all_candidates, key=lambda x: x[1], reverse=True)
+
+        # Prune the sequences to keep only the top k beam
+        sequences, scores = zip(*ordered[:num_beams])
+
+        # If all of the sequences are complete, break out of the loop
+        if all(seq[-1] == end_token for seq in sequences):
+          break
+
+    # Take the sequence with the highest score
+    best_sequence = sequences[0]
+
+    # Decode the tokens
+    words = [index_to_word[token] for token in best_sequence if token not in [0, start_token, end_token]]
+    caption = ' '.join(words)
+    return caption, [layer.last_attention_scores for layer in captioning_model.decoder_block]
+
 
 def show_attention_maps(attention_maps, image, caption):
     # Prepare the image
@@ -85,21 +153,12 @@ def show_attention_maps(attention_maps, image, caption):
     # Split the caption into tokens
     tokens = caption.split()
 
-    # fig = plt.figure(figsize=(25, 10))
-    # grid_size = max(int(np.ceil(len(tokens)/4)), 4)
-    # num_tokens = len(tokens)
-    # rows = 4
-    # cols = int(np.ceil(num_tokens / rows)) + 1
-
-    num_tokens = len(tokens)
     cols = 3
     rows = int(np.ceil(len(tokens) / cols))
 
     fig, axes = plt.subplots(rows, cols, figsize=(20, rows * 5), constrained_layout=True)
 
     axes = axes.ravel()
-
-    # titles = []
 
     for i in range(len(tokens)):
         # Get the averaged attention map for this token
@@ -114,18 +173,6 @@ def show_attention_maps(attention_maps, image, caption):
         # Set the title and remove axes
         axes[i].set_title(tokens[i], fontsize=30)
         axes[i].set_axis_off()
-
-    #     # Label this subplot with its token
-    #     ax = fig.add_subplot(rows, cols, i+1)
-    #     titles.append(ax.set_title(tokens[i]))
-
-    #     # Plot the image
-    #     img = ax.imshow(resized_image)
-
-    #     # Plot the attention map as a transparent heatmap over the image
-    #     ax.imshow(map,  cmap='gray', alpha=0.7, extent=img.get_extent(), clim=[0.0, np.max(map)])
-    #     ax.set_axis_off()
-    # fig.tight_layout()
 
     for i in range(len(tokens), len(axes)):
         axes[i].axis("off")
